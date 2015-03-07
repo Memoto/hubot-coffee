36d842cd55e8dbb34a58361209e8869908db341a
Joakim Berglund <pusselgenerator@gmail.com>
2015-03-07 14:50:44 +0100

    more stuff

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var data = {};
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send(data);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/brew_hook', function(request, response) {
   console.log(request.body);
+  io.emit('brew_update', request.body);
+
   data = request.body.event + " " + request.body.data;
   response.sendStatus(200);
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
