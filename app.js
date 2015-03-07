e05fb792d88150ad7d06a5cdf5fb7812c804a28d
Joakim Berglund <pusselgenerator@gmail.com>
2015-03-07 14:20:23 +0100

    Some work

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var data = {};
 
+app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(data);
+});
+
+app.post('/brew_hook', function(request, response) {
+  data = request.body;
+  response.sendStatus(200);
 });
 
 app.listen(app.get('port'), function() {
