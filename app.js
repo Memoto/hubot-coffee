b3ff87961db8c1e5e5e5dccbdb71601a035d5043
Joakim Berglund <pusselgenerator@gmail.com>
2015-03-07 15:03:19 +0100

    work

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ io.on('connection', function(socket){
   });
 });
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
